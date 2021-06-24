f00399d66c728ac1732d637e8e636d05d7f70fa7
TGF0911 <gennari0911@gmail.com>
2021-06-24 00:57:07 -0300

    Aula04 -React(Added Questions and AdminPage)

--- letmeask/src/pages/Home.tsx
+++ letmeask/src/pages/Home.tsx
@@ -34,7 +34,11 @@ export function Home() {
       alert('Room does not exists')
       return
     }
-
+    
+    if((await roomRef).val().endedAt){
+      alert('Room already closed')
+      return
+    }
     history.push(`/rooms/${roomCode}`)
   }
 
